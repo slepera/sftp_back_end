82c6d22ecad0b0ea5221e00cf8b9cb4b29364d45
Salvatore Lepera <salvatore.lepera.lavoro@gmail.com>
2021-10-01 10:10:36 +0200

    added rest api to test mysql document store

--- src/main/java/fds/sftp/SFTPRestController.java
+++ src/main/java/fds/sftp/SFTPRestController.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-
+import com.mysql.cj.xdevapi.*;
 @RestController
 @RequestMapping("/sftp_client")
 public class SFTPRestController {
@@ -20,5 +20,23 @@ public class SFTPRestController {
     public void SFTPClientGet(@PathVariable("id") String id) throws IOException {
         sftpClient.GetFile(id);
     }
+
+    @GetMapping ("/mysql_document_store")
+    public String ProvaMySQL() throws IOException {
+        Session mySession = new SessionFactory().getSession("mysqlx://uqiqrf3ae3eo7tep:W1uRj2qfAzukKLgMLDXr@b7c6bxkiztedyuahpq3l-mysql.services.clever-cloud.com:33060/b7c6bxkiztedyuahpq3l");
+        //Session mySession = new SessionFactory().getSession("mysqlx://b7c6bxkiztedyuahpq3l-mysql.services.clever-cloud.com:3306/b7c6bxkiztedyuahpq3l?user=uqiqrf3ae3eo7tep&password=W1uRj2qfAzukKLgMLDXr");
+
+        Schema myDb = mySession.getSchema("b7c6bxkiztedyuahpq3l");
+
+        Collection myColl = myDb.getCollection("countryinfo");
+
+        //DocResult myDocs = myColl.find("name like :param").limit(1).bind("param", "L%").execute();
+        DocResult myDocs = myColl.find().execute();
+        String res = myDocs.fetchOne().toString();
+
+        mySession.close();
+        return res;
+    }
+
 
 }
